d5ad68ed72645b459b47bfdb623a998c33846235
learningWithSecondAccount <mustafamoham4646@gmail.com>
2026-05-13 02:44:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 // does any one can just throw code 
 // this comment is not going to be pushed to the main branch unless the admin approve it 
+// this comment is new and it is written by collaborator 
+
 
 void fun(FILE *p, int r, int x , char namee[30], char *name[20], int *Quantity, long double *price);
 struct Item
