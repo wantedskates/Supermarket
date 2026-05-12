fb8b3394f79ba960964ae9bd47c5d9614a1068c7
learningWithSecondAccount <mustafamoham4646@gmail.com>
2026-05-13 02:50:39 +0300

    i want to add a comment

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 // does any one can just throw code 
 // this comment is not going to be pushed to the main branch unless the admin approve it 
 // this comment is new and it is written by collaborator 
-
+// if this comment was delayed , this mean that this stuff had worked 
 
 void fun(FILE *p, int r, int x , char namee[30], char *name[20], int *Quantity, long double *price);
 struct Item
