13609619291b8860cf24582128ec0a9253a38762
Mustafa Mohamed <Mustafamoham46@gmail.com>
2026-05-13 03:35:22 +0300

    Clean up comments in main.c
    Remove unnecessary comments from main.c

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-// does any one can just throw code 
-// this comment is not going to be pushed to the main branch unless the admin approve it 
-// this comment is new and it is written by collaborator 
+
 
 
 void fun(FILE *p, int r, int x , char namee[30], char *name[20], int *Quantity, long double *price);
